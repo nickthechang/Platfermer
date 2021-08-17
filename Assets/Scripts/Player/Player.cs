8a11259c5ca271750772a6ae2fbe35c2ad722a15
tannnguyen13 <tann2000@gmail.com>
2021-08-17 12:56:06 -0700

    did more than just jump

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    public float timeToJumpApex = .4f; //how long will the char will jump for
+    public float maxJumpHeight = 4; //how high the character jump
+    public float minJumpHeight = 1;
+    float accelerationTimeAirbone = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 6;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timeToWallUnstick;
+
+    float gravity = -20;
+    Vector3 velocity;
+    float maxJumpVelocity = 8;
+    float minJumpVelocity;
+    float velocityXSmoothing;
+
+    Vector2 directionalInput;
+    bool wallSliding;
+    int wallDirX;
+
+    Controller2D controller;
+    private void Start()
+    {
+        controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        print("Gravity: " + gravity + "  Jump Velocity: " + maxJumpVelocity); 
+    }
+    private void Update()
+    {
+        CalculateVelocity();
+        HandleWallSliding();
+        controller.Move(velocity * Time.deltaTime, directionalInput);
+
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+    }
+
+    public void SetDirectionalInput(Vector2 input)
+    {
+        directionalInput = input;
+    }
+
+    public void OnJumpInputDown()
+    {
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+
+        }
+        if (controller.collisions.below)
+        {
+            velocity.y = maxJumpVelocity;
+            
+        }
+        controller.anim.SetTrigger("Jump");
+    }
+
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+            velocity.y = minJumpVelocity;
+    }
+
+    void HandleWallSliding()
+    {
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+        wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+        {
+            controller.anim.SetBool("wall", true);
+            //transform.localScale = transform.localScale * -1;
+            GetComponent<SpriteRenderer>().flipX = true;
+            wallSliding = true;
+            if (velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+            if (timeToWallUnstick > 0)
+            {
+                velocityXSmoothing = 0;
+                velocity.x = 0;
+
+                if (directionalInput.x != wallDirX && directionalInput.x != 0)
+                {
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timeToWallUnstick = wallStickTime;
+                }
+            }
+            else
+            {
+                
+                timeToWallUnstick = wallStickTime;
+
+            }
+        }
+        if(!wallSliding)
+        {
+            controller.anim.SetBool("wall", false);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+            
+    }
+
+    void CalculateVelocity()
+    {
+        float targetVelocityX = directionalInput.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirbone);
+        velocity.y += gravity * Time.deltaTime;
+    }
+}
